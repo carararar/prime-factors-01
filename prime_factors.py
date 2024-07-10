a4a31004a3854df50573b83b0ffedaff691c8065
car <znargon@gmail.com>
2024-07-10 09:11:51 +0900

    [feature] p39(green level)

--- prime_factors.py
+++ prime_factors.py
@@ -1,29 +1,20 @@
 class PrimeFactors:
     def of(self, n) -> []:
         factors = []
+        if n == 1:
+            return factors
+        while n > 1:
+            if n % 2 == 0:
+                factors.append(2)
+                n /= 2
+            elif n % 7 == 0:
+                factors.append(7)
+                n /= 7
+            elif n % 5 == 0:
+                factors.append(5)
+                n /= 5
+            elif n % 3 == 0:
+                factors.append(3)
+                n /= 3
 
-        if n == 2:
-            factors.append(2)
-        if n == 3:
-            factors.append(3)
-        if n == 4:
-            factors.append(2)
-            factors.append(2)
-        if n == 6:
-            factors.append(2)
-            factors.append(3)
-        if n == 8:
-            factors.append(2)
-            factors.append(2)
-            factors.append(2)
-        if n == 9:
-            factors.append(3)
-            factors.append(3)
-        if n == 12:
-            factors.append(2)
-            factors.append(2)
-            factors.append(3)
-        if n == 14:
-            factors.append(2)
-            factors.append(7)
         return factors
