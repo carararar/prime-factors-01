b808cf5edabdf52a4ac824f8f034f42d1eaca7ae
car <znargon@gmail.com>
2024-07-10 09:18:36 +0900

    [refactoring] p50

--- prime_factors.py
+++ prime_factors.py
@@ -1,20 +1,12 @@
 class PrimeFactors:
-    def of(self, n) -> []:
+    def of(self, number) -> []:
         factors = []
-        if n == 1:
+        if number == 1:
             return factors
-        while n > 1:
-            if n % 2 == 0:
-                factors.append(2)
-                n /= 2
-            elif n % 7 == 0:
-                factors.append(7)
-                n /= 7
-            elif n % 5 == 0:
-                factors.append(5)
-                n /= 5
-            elif n % 3 == 0:
-                factors.append(3)
-                n /= 3
-
+        divisor = 2
+        while number > 1:
+            while number % divisor == 0:
+                factors.append(divisor)
+                number //= divisor
+            divisor += 1
         return factors
