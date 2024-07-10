58dec22fc7d4810fbbaaf68a9282734a02c4c268
car <znargon@gmail.com>
2024-07-10 09:00:18 +0900

    [feature] p31(green level)

--- prime_factors.py
+++ prime_factors.py
@@ -1,2 +1,29 @@
 class PrimeFactors:
-    pass
+    def of(self, n) -> []:
+        factors = []
+
+        if n == 2:
+            factors.append(2)
+        if n == 3:
+            factors.append(3)
+        if n == 4:
+            factors.append(2)
+            factors.append(2)
+        if n == 6:
+            factors.append(2)
+            factors.append(3)
+        if n == 8:
+            factors.append(2)
+            factors.append(2)
+            factors.append(2)
+        if n == 9:
+            factors.append(3)
+            factors.append(3)
+        if n == 12:
+            factors.append(2)
+            factors.append(2)
+            factors.append(3)
+        if n == 14:
+            factors.append(2)
+            factors.append(7)
+        return factors
--- test_prime_factors.py
+++ test_prime_factors.py
@@ -9,4 +9,28 @@ class TestPrimeFactors(TestCase):
         self.sut = PrimeFactors()
 
     def test_prime_factors_when_1(self):
-        self.assertEqual([], [])
+        self.assertEqual([], self.sut.of(1))
+
+    def test_prime_factors_when_2(self):
+        self.assertEqual([2], self.sut.of(2))
+
+    def test_prime_factors_when_3(self):
+        self.assertEqual([3], self.sut.of(3))
+
+    def test_prime_factors_when_4(self):
+        self.assertEqual([2, 2], self.sut.of(4))
+
+    def test_prime_factors_when_6(self):
+        self.assertEqual([2, 3], self.sut.of(6))
+
+    def test_prime_factors_when_8(self):
+        self.assertEqual([2, 2, 2], self.sut.of(8))
+
+    def test_prime_factors_when_9(self):
+        self.assertEqual([3, 3], self.sut.of(9))
+
+    def test_prime_factors_when_12(self):
+        self.assertEqual([2, 2, 3], self.sut.of(12))
+
+    def test_prime_factors_when_14(self):
+        self.assertEqual([2, 7], self.sut.of(14))
